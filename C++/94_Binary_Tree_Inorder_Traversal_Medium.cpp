f58ad9569e2018870b5b0d0a30d876d89ae9cb25
Bing <bing@bingw.me>
2019-02-24 21:31:28 +0800

    leetcode 94

---
+++ C++/94_Binary_Tree_Inorder_Traversal_Medium.cpp
@@ -0,0 +1,30 @@
+#include "lib.h"
+using namespace std;
+
+class Solution {
+public:
+    void helper(TreeNode* root) {
+        stack<TreeNode*> treeStack;
+        while (!treeStack.empty() || root != NULL) {
+            while (root) {
+                // 一路向左
+                treeStack.push(root);
+                root = root->left;
+            }
+            // 此时root已经为空，指向最左边的外边缘
+            // 是时候取出来访问了：取出--访问--弹出三位一体
+            root = treeStack.top();
+            res.push_back(root->val);
+            treeStack.pop();
+
+            // 向右
+            root = root->right;
+        }
+    }
+    vector<int> inorderTraversal(TreeNode* root) {
+        helper(root);
+        return res;
+    }
+private:
+    vector<int> res;
+}
